e3aa330d1dfa465f5758987f0e34232b4c11e318
Guizaoo <guilhermexxzz54@gmail.com>
2026-02-19 14:16:34 -0300

    Adicao de atividades

---
+++ untitled/src/Comparativos.java
@@ -0,0 +1,16 @@
+public class Comparativos {
+    static void main(String[] args) {
+
+        //  && ele precisa q os 2 sejam verdadeiros para que me retorne True
+
+        // || OR ele precisa que apenas 1 seja vadadeiro para retornar true caso so dois sejam false ele retorna false
+        int idade = 23;
+        float salrio = 4500F;
+
+        boolean isMaiorQue20 = idade >= 23 && salrio >= 4000;
+        boolean isSalarioMaior = idade >= 23 && salrio >= 5000;
+
+        System.out.println(isMaiorQue20);
+        System.out.println(isSalarioMaior);
+    }
+}
---
+++ untitled/src/atribuicao.java
@@ -0,0 +1,14 @@
+public class atribuicao {
+    static void main(String[] args) {
+
+        // = += -= *= /= %= formatos de abreviacao pro codigo n ficar tao extenço
+
+    double bonus = 1800;
+    bonus += 1000;
+    bonus -= 1000;
+
+
+        System.out.println(bonus);
+
+    }
+}
